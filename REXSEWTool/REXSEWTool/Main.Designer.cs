d1e9a77587a337948e5154b283b8ba0093f64d2b
Lucas <madneshell@outlook.com>
2020-11-06 19:19:46 -0300

    Update Main.Designer.cs

--- REXSEWTool/REXSEWTool/Main.Designer.cs
+++ REXSEWTool/REXSEWTool/Main.Designer.cs
@@ -1,4 +1,18 @@
-﻿namespace REXSEWTool
+/*
+This file is part of RESIDENT EVIL XSEW Tool.
+RESIDENT EVIL XSEW Tool is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+RESIDENT EVIL XSEW Tool is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with RESIDENT EVIL XSEW Tool. If not, see <https://www.gnu.org/licenses/>6.
+*/
+
+namespace REXSEWTool
 {
     partial class Main
     {
